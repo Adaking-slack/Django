5cad44c78216928c89ed0019ac6d0960e87e4473
Emelike Adaeze <emelikepatience961@gmail.com>
2022-06-20 17:32:47 +0100

    made some corrections

--- I4G00348432N/Blog/models.py
+++ I4G00348432N/Blog/models.py
@@ -7,7 +7,7 @@ User=get_user_model()
 
 class Post(models.Model):
      Title = models.CharField(max_length=200)
-     Content= models.TextField()
+     Text= models.TextField()
      Created_date = models.DateTimeField()
      Published_date  = models.DateTimeField()
      Author = models.ForeignKey(User, on_delete=models.CASCADE,  null=True)
